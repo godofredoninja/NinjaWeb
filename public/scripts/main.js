64adf47b04c4c862e17218bfd31f22664b6baee9
GodoFredo <godofredo720@hotmail.com>
2014-12-03 11:24:29 -0500

    Realizando el Menu

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,42 @@
 (function(){
 	'use strict';
 
-	console.log('hola');
+	// Menu Mobile
+	var menu_active = false;
+
+	$('.Header-iconMobile').on('click', menuMobile);
+
+	var $body = document.querySelector('.wrapers');
+	var body = new Hammer($body);
+
+	body.on('panright', function(){
+		$('.moveLeft').removeClass('active');
+		$('.MainMenu').removeClass('active');
+	});
+
+	function menuMobile(){
+		if(!menu_active){
+			$('.moveLeft').addClass('active');
+			$('.MainMenu').addClass('active');
+			menu_active = true;
+		}else{
+			$('.moveLeft').removeClass('active');
+			$('.MainMenu').removeClass('active');
+			menu_active = false;
+		}
+	}
+
+	// Menu se detiene
+		var altura = $('.Header').offset().top;
+
+		$(window).scroll(function(){
+			if($(window).scrollTop() >= altura){
+				$('.Header').css('position','fixed');
+			}else{
+				$('.Header').css('position','relative');
+			}
+		});
+
 
 
 
