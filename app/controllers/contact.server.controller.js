2e25064f6c0110e71ac76f2f313cf70e7e121b25
GodoFredo <godofredo720@hotmail.com>
2015-01-27 16:58:24 -0500

    Agregando mail

--- app/controllers/contact.server.controller.js
+++ app/controllers/contact.server.controller.js
@@ -1,67 +1,39 @@
 // Invocar modo JavaScript 'strict'
 'use strict';
-
 var nodemailer = require('nodemailer');
 
-
 // Crear un nuevo método controller 'renderSendMessaje'
-
 exports.renderSendMessage = function(req, res, next) {
 
-    var data = {};
-    data = JSON.stringify(req.body);
-    console.log(data);
-    res.end('1');
+    // create reusable transporter object using SMTP transport
+    var transporter = nodemailer.createTransport({
+        service: "Mailgun",
+        auth: {
+            user: 'web@mg.godofredo.ninja',
+            pass: 'xxx'
+        }
+    });
 
-    // // create reusable transporter object using SMTP transport
-    // var transporter = nodemailer.createTransport('SMTP', {
-    //  host: 'smtp.mandrillapp.com',
-    //  port: 587,
-    //  // service: 'Mandrill',
-    //  auth: {
-    //      user: 'godofredo207@gmail.com',
-    //      pass: 'xxx'
-    //  }
-    // });
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: req.body.name +'<web@mg.godofredo.ninja>', // sender address
+        to: 'hello@godofedo.ninja', // list of receivers
+        subject: 'Mail Ninja Web', // Subject line
+        html: ''
+        + '<p> Nombre: '+ req.body.name +'</p>'
+        + '<p> Email: '+ req.body.email +'</p>'
+        + '<p> Telefono: '+ req.body.phone +'</p>'
+        + '<p> Empresa: '+ req.body.company +'</p>'
+        + '<p> Mensage: '+ req.body.message +'</p>'
+    };
 
-    // // setup e-mail data with unicode symbols
-    // var mailOptions = {
-    //  from: req.body.name+'<godofredo207@gmail.com>', // sender address
-    //  to: 'godofrodo20@gmail.com', // list of receivers
-    //  subject: 'Hello ', // Subject line
-    //  text: 'Hello world ', // plaintext body
-    //  html: '<b>'+req.body.message+'</b>' // html body
-    // };
-
-    // // send mail with defined transport object
-    // transporter.sendMail(mailOptions, function(error, info){
-    //  if(error){
-    //      // res.end(0);
-    //      console.log(error);
-    //  }else{
-    //      res.end(1);
-    //  }
-    // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            // console.log(error);
+            res.end("0");
+        }else{
+            res.end("1");
+        }
+    });
 };
-
-
-
-
-
-
--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,93 +1,85 @@
 (function(){
+    'use strict';
 
-	'use strict';
+    $('#Form').on('submit', SendData);
 
-	$('#Form').on('submit', SendData);
+    var
+        emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i),
 
-	var
-		emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i),
+        $username       = $('#name'),
+        failedmessage   = 'Hay un problema, por favor intente de nuevo',
+        $useremail      = $('#email'),
+        $formMessage    = $('.Form-message'),
+        isvalid         = 1,
+        data            = {},
+        url             = "/contacto";
 
-		successmessage	= "Gracias " + $('#name').val() + ", me pondre en contacto cuanto antes posible",
-		failedmessage	= "Hay un problema, por favor intente de nuevo",
-		$username 		= $('#name'),
-		$useremail		= $('#email'),
-		$formMessage	= $('.Form-message'),
-		isvalid			= 1,
-		data			= {},
-		url				= "/contacto";
+    function SendData(e){
 
-	function SendData(e){
+        var successmessage  = 'Gracias '+ $username.val() +', me pondre en contacto cuanto antes posible';
 
-		if($username.val() === ""){
-			$username.addClass('active');
-			$formMessage
-				.addClass('active')
-				.html('Escribe tu Nombre');
-			return false;
-		}
+        if($username.val() === ""){
+            $username.addClass('active');
+            $formMessage
+                .addClass('active')
+                .html('Escribe tu Nombre');
+            return false;
+        }
 
-		if($useremail.val() === ""){
-			$useremail.addClass('active');
-			$formMessage
-				.addClass('active')
-				.html('Escribe tu Correo');
-			return false;
-		}
+        if($useremail.val() === ""){
+            $useremail.addClass('active');
+            $formMessage
+                .addClass('active')
+                .html('Escribe tu Correo');
+            return false;
+        }
 
-		var valid = emailReg.test($useremail.val());
+        var valid = emailReg.test($useremail.val());
 
-		if(!valid){
-			$useremail.addClass('active');
-			$formMessage
-				.addClass('active')
-				.html('Introduce un Correo Valido');
-			return false;
-		}
+        if(!valid){
+            $useremail.addClass('active');
+            $formMessage
+                .addClass('active')
+                .html('Introduce un Correo Valido');
+            return false;
+        }
 
-		data = $(this).serializeArray();
-
-		$.ajax({
-			type: "POST",
-			url:"/contacto",
-			data: data,
-			dataType: "json",
-			success: function(res){
-				if(res === isvalid){
-					$formMessage.addClass('active');
-					$formMessage.html(successmessage);
-				} else {
-					$formMessage.addClass('active');
-					$formMessage.html(failedmessage)
-				}
-			}
-		});
-
-		$('#Form input[type="text"],textarea').val('');
-
-		e.preventDefault();
-	};
+        data = $(this).serializeArray();
 
 
-	$username.focus(function(){
-		$username.removeClass('active');
-		$formMessage
-			.removeClass('active')
-			.html('');
-	});
+        $.ajax({
+            type: "POST",
+            url:"/contacto",
+            data: data,
+            dataType: "json",
+            success: function(res){
+                if(res === isvalid){
+                    $formMessage.addClass('active');
+                    $formMessage.html(successmessage);
+                } else {
+                    $formMessage.addClass('active');
+                    $formMessage.html(failedmessage)
+                }
+            }
+        });
+        $('#Form input[type="text"],textarea').val('');
+        e.preventDefault();
+    };
 
-	$useremail.focus(function(){
-		$useremail.removeClass('active');
-		$formMessage
-			.removeClass('active')
-			.html('');
-	});
 
+    $username.focus(function(){
+        $username.removeClass('active');
+        $formMessage
+            .removeClass('active')
+            .html('');
+    });
 
-
-
-
-
-
+    $useremail.focus(function(){
+        $useremail.removeClass('active');
+        $formMessage
+            .removeClass('active')
+            .html('');
+    });
 
 
 })();
