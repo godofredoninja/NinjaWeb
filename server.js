983196737ab2cd8d405fa6b333412369541dbe92
GodoFredo <godofredo720@hotmail.com>
2015-01-29 13:19:32 -0500

    solu error

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 app.listen(3000);
 
 // Hacer Log del status del server a la consola
-console.log('servidor ejecutandose en => localhost:3000');
+// console.log('servidor ejecutandose en => localhost:3000');
 
 // Usar la prpiedad module.exports para exponer nuestra nuestra instancia de la aplicación Express para uso externo
 module.exports = app;
